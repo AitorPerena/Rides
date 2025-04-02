c2efd87afd18567914376873eaaf850ba3434b50
aitor <aitor@DESKTOP-O2PAE1U>
2025-04-02 17:48:05 +0200

    Proyecto tras iteración 2 con idiomas

--- Rides24/src/main/java/gui/RequestRideGUI.java
+++ Rides24/src/main/java/gui/RequestRideGUI.java
@@ -1,10 +1,19 @@
 package gui;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
 import businessLogic.BLFacade;
 import domain.Ride;
 import domain.Traveler;
@@ -15,46 +24,44 @@ public class RequestRideGUI extends JFrame {
     private JSpinner jSpinnerSeats;
     private Traveler traveler;
     private ResourceBundle bundle;
-    private SimpleDateFormat dateFormat;
 
     public RequestRideGUI(Traveler traveler) {
-        this.traveler = traveler;
         this.bundle = ResourceBundle.getBundle("Etiquetas");
-        this.dateFormat = new SimpleDateFormat(bundle.getString("Date.Format"));
-        
+        this.traveler = traveler;
         setTitle(bundle.getString("RequestRideGUI.Title"));
-        setSize(400, 200);
-        setLayout(new GridLayout(4, 2));
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(400, 200); // Tamaño ajustado
+        setLayout(new GridLayout(4, 2)); 
 
-        initializeUI();
-    }
-
-    private void initializeUI() {
         // Lista de viajes disponibles
-        add(new JLabel(bundle.getString("RequestRideGUI.SelectRide")));
+        add(new JLabel(bundle.getString("RequestRideGUI.Seats")));
         jComboBoxRides = new JComboBox<>();
         BLFacade facade = MainGUI.getBusinessLogic();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy"); 
+
         
         for (Ride ride : facade.getAvailableRides()) {
-            String rideInfo = String.format("%s - %s (%s)", 
-                ride.getFrom(), 
-                ride.getTo(), 
-                dateFormat.format(ride.getDate()));
+            String rideInfo = ride.getFrom() + " - " + ride.getTo() + " (" + dateFormat.format(ride.getDate()) + ")";
             jComboBoxRides.addItem(rideInfo);
         }
         add(jComboBoxRides);
 
         // Selección de número de asientos
         add(new JLabel(bundle.getString("RequestRideGUI.Seats")));
-        jSpinnerSeats = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
+        jSpinnerSeats = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1)); 
         add(jSpinnerSeats);
 
-        // Botón de solicitud
+  
         jButtonRequest = new JButton(bundle.getString("RequestRideGUI.Submit"));
-        jButtonRequest.addActionListener(e -> requestReservation());
+        jButtonRequest.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                requestReservation();
+            }
+        });
         add(jButtonRequest);
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
+
 
     private void requestReservation() {
         BLFacade facade = MainGUI.getBusinessLogic();
@@ -66,23 +73,12 @@ public class RequestRideGUI extends JFrame {
 
             boolean success = facade.requestReservation(selectedRide, traveler, seats);
             if (success) {
-                facade.sendRideReminders(selectedRide.getDate());
-                JOptionPane.showMessageDialog(this, 
-                    bundle.getString("RequestRideGUI.Success"),
-                    bundle.getString("Success"), 
-                    JOptionPane.INFORMATION_MESSAGE);
-                dispose();
+                JOptionPane.showMessageDialog(this, bundle.getString("RequestRideGUI.Success"));
             } else {
-                JOptionPane.showMessageDialog(this, 
-                    bundle.getString("RequestRideGUI.Error"),
-                    bundle.getString("Error"), 
-                    JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, bundle.getString("RequestRideGUI.Error"));
             }
         } else {
-            JOptionPane.showMessageDialog(this, 
-                bundle.getString("RequestRideGUI.InvalidSelection"),
-                bundle.getString("Error"), 
-                JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, bundle.getString("RequestRideGUI.InvalidSelection"));
         }
     }
 }
